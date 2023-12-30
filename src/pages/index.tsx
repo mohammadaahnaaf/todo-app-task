08eab36b9dd5985ebb784d2ed5eda60baa7b856c
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-12-30 21:05:00 +0600

    feat: :fire: delete task

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import React from 'react';
-import { getTasks } from '@ahnafya/utils';
+import { deleteTask, getTasks } from '@ahnafya/utils';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,21 +10,38 @@ export default function HomePage() {
 
   const [tasks, setTasks] = React.useState<any[]>([])
   const [total, setTotal] = React.useState<number>(0)
+  const [success, setSuccess] = React.useState<string>('')
 
+
+  // Get All Tasks
   React.useEffect(() => {
     const fetchData = async () => {
       try {
-        const tasks = await getTasks();
-        setTasks(tasks.tasks)
-        setTotal(tasks.count)
-        console.log('Tasks:', tasks);
+        const tasksq = await getTasks();
+        setTasks(tasksq.tasks)
+        setTotal(tasksq.count)
+        console.log('Tasks:', tasksq);
       } catch (error) {
         console.error('Error fetching tasks:', error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [success]);
+
+  // Delete Task
+  async function handleDelete(id: string) {
+    try {
+      const resp = await deleteTask(id);
+      setSuccess('Task Deleted')
+      setTimeout(() => {
+        setSuccess('')
+      }, 2000)
+      console.log('Tasks:', resp);
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  }
 
   return (
     <>
@@ -43,8 +60,17 @@ export default function HomePage() {
         <div className='max-w-5xl mx-auto w-full p-4'>
           <div className='grid gap-4 items-center w-full'>
             {tasks?.map((x: any, index: number) => (
-              <div key={index} className={`rounded-lg ${x.completed ? "bg-[green]" : " bg-[blue]"} hover:bg-black duration-300 hover:scale-95 cursor-pointer p-4`}>
-                <p>{x.task}</p>
+              <div key={index} className={`rounded-lg flex justify-between gap-2 ${x.completed ? "bg-[green]" : " bg-[blue]"} hover:bg-black duration-300 hover:scale-95 cursor-pointer p-4`}>
+                <div>
+                  <p>{index + 1}. {x.task}</p>
+                  <p>{index + 1}. {x.task}</p>
+                </div>
+                <div className='flex gap-2 items-end'>
+                  <button className='bg-sky-600 px-3 py-1 rounded-md ring-1 ring-white'>Edit</button>
+                  <button className='bg-[red] px-3 py-1 rounded-md ring-1 ring-white'
+                    onClick={() => handleDelete(x.id)}
+                  >Delete</button>
+                </div>
               </div>
             ))}
           </div>
--- src/utils/apiCall.ts
+++ src/utils/apiCall.ts
@@ -5,11 +5,22 @@ import axios from 'axios';
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getTasks = async () => {
-  try {
-    const response = await axios.get(`${BASE_URL}/tasks`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching tasks:', error);
-    throw error;
-  }
+    try {
+        const response = await axios.get(`${BASE_URL}/tasks`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching tasks:', error);
+        throw error;
+    }
+};
+
+export const deleteTask = async (id: string) => {
+    try {
+        const response = await axios.delete(`${BASE_URL}/tasks/${id}`);
+        getTasks()
+        return response.data;
+    } catch (error) {
+        console.error('Error deleting tasks:', error);
+        throw error;
+    }
 };
